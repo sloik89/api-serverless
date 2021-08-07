caba2029aa7050aa904a2c6b23ebfab53daa8305
sloik89 <sloik89@gmail.com>
2021-08-07 19:03:15 +0200

    complete

--- functions/product.js
+++ functions/product.js
@@ -18,6 +18,9 @@ exports.handler = async(event,constex) => {
                 }
             }
                 return{
+                    headers:{
+            'Access-Control-Allow-Origin':'*'
+        },
                     statusCode:200,
                     body:JSON.stringify(product)
                 }
